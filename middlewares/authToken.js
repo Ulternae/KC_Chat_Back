f1eba423b090c5640e0b54a52c9aae8e4b85ef82
Ulternae <ulternae@gmail.com>
2024-06-10 17:26:39 -0500

    Add types error for tokens

--- middlewares/authToken.js
+++ middlewares/authToken.js
@@ -3,14 +3,14 @@ import { verifyToken } from "../utils/jwt.js";
 const authToken = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "Access denied" });
+  if (!token) return res.status(401).json({ message: "Access denied", type: 'accessDenied' });
 
   try {
     const decoded = verifyToken({ token });
     req.user = decoded; // en la request se le manda los datos del usuario???
     next();
   } catch (error) {
-    res.status(403).json({ message: "Invalid token" });
+    res.status(403).json({ message: "Invalid token" , type: 'invalidToken'});
   }
 };
 
